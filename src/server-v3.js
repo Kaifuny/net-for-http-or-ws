1f9975749d4edba1082a03fc378a44f2406b3c87
Kaifuny <superbiger.github@gmail.com>
2023-05-20 21:14:11 +0800

    add: simple websocket server

--- src/server-v3.js
+++ src/server-v3.js
@@ -3,3 +3,24 @@
  * Author: Kaifuny
  * LICENSE: MIT
  */
+const { createServer } = require("net");
+
+const server = createServer((socket) => {
+  socket.once("data", (data) => {
+    if (data.match(/Upgrade: websocket/)) {
+      // TODO: handle websocket
+    }
+  });
+
+  socket.on("data", (data) => {
+    console.log("socket data", data.toString());
+  });
+});
+
+server.on("connection", (data) => {
+  console.log("server connection", data.toString());
+});
+
+server.listen(Config.port, Config.host, () => {
+  console.log("server is running on port 8888");
+});
